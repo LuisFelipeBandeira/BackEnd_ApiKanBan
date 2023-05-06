91c7a637281e70ddc49c48012ed4a7d2b6296943
Luis Felipe Silva Bandeira <107070986+LuisFelipeBandeira@users.noreply.github.com>
2023-05-06 17:10:11 -0300

    Init routes created

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"Golang/BackEnd_ApiKanBan/routes"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	routes.InitRoutes(&router.RouterGroup)
 }
